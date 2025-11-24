4af7531627cb47b72c6f4f84b2b212b00da99210
Heyson <Heysonpolanco65@gmail.com>
2025-11-23 23:31:47 -0400

    Added missing models

---
+++ FinalProyect/Models/DerechoEnterramiento.cs
@@ -0,0 +1,35 @@
+﻿using FinalProyect.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProyect.Models;
+
+public class DerechoEnterramiento
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string UsuarioId { get; set; } = string.Empty;
+    public ApplicationUser Usuario { get; set; }
+
+    [Required]
+    public int SolicitanteId { get; set; }
+    public Solicitante Solicitante { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string NombreFallecido { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(20)]
+    public string CedulaFallecido { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Fecha { get; set; }
+
+    [Required]
+    public TimeSpan Hora { get; set; }
+
+    [Required]
+    public int ReciboIngresoId { get; set; }
+    public ReciboIngreso ReciboIngreso { get; set; }
+}
