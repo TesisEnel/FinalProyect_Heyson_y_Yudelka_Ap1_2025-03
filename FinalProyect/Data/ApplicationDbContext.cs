a84b20a500c0a467b265f9ebf2623a937d448597
Heyson <Heysonpolanco65@gmail.com>
2025-11-23 23:30:56 -0400

    Modifications with the proceses

--- FinalProyect/Data/ApplicationDbContext.cs
+++ FinalProyect/Data/ApplicationDbContext.cs
@@ -7,49 +7,23 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<ReciboIngreso> ReciboIngreso { get; set; }
     public DbSet<Solicitante> Solicitantes { get; set; }
-    public DbSet<Proceso> Procesos { get; set; }
+    public DbSet<ArrendamientoTerreno> ArrendamientoTerreno { get; set; }
     public DbSet<Documento> Documentos { get; set; }
     public DbSet<Historial> Historiales { get; set; }
     public DbSet<ConsultaIngresos> ConsultasIngresos { get; set; }
-
+    public DbSet<DerechoConstruccion> DerechoConstruccion { get; set; }
+    public DbSet<ExpedicionCertificacion> ExpedicionesCertificaciones { get; set; }
+    public DbSet<DerechoEnterramiento> DerechoEnterramiento { get; set; }
+    public DbSet<RegistroDocumentacion> RegistrosDocumentacion { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<ReciboIngreso>()
-               .HasOne(r => r.Solicitante)
-               .WithMany(s => s.RecibosIngresos)
-               .HasForeignKey(r => r.SolicitanteId)
-               .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<ReciboIngreso>()
-               .HasOne(r => r.Documento)
-               .WithMany(d => d.RecibosIngresos)
-               .HasForeignKey(r => r.DocumentoId)
-               .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<ReciboIngreso>()
-               .HasOne(r => r.Proceso)
-               .WithMany(p => p.RecibosIngresos)
-               .HasForeignKey(r => r.ProcesoId)
-               .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<Historial>()
-               .HasOne(h => h.ReciboIngreso)
-               .WithMany(r => r.Historiales)
-               .HasForeignKey(h => h.ReciboIngresoId)
-               .OnDelete(DeleteBehavior.Cascade);
-
-        builder.Entity<Historial>()
-               .HasOne(h => h.Usuario)
-               .WithMany(u => u.Historiales)
-               .HasForeignKey(h => h.usuarioId)
-               .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<Historial>()
-               .HasOne(h => h.Proceso)
-               .WithMany(p => p.Historiales)
-               .HasForeignKey(h => h.ProcesoId)
-               .OnDelete(DeleteBehavior.Restrict);
+        foreach (var relationship in builder.Model
+            .GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
     }
 }
