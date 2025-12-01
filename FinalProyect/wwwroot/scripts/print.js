398997a3bf3d42745f609e363710df0cb96c210d
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-12-01 00:19:50 -0400

    feat(print): Agregando script global print.js para manejo de impresión.

---
+++ FinalProyect/wwwroot/scripts/print.js
@@ -0,0 +1,13 @@
+﻿window.printSection = (elementoId) {
+    const original = document.body.innerHTML;
+    const section = document.getElementById(elementoId).innerHTML;
+
+    document.body.innerHTML = `
+        <div style="padding: 40px;">
+            ${section}
+        </div>
+    `;
+    window.print();
+    document.body.innerHTML = original;
+    location.reload();
+};
